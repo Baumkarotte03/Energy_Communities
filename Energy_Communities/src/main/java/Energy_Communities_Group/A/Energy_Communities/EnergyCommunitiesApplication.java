49896c4c2df21854b1a62270a8ca4487b9093302
Lukas Hahn <l.hahn785@gmail.com>
2026-04-23 18:49:39 +0200

    Initial commit

---
+++ Energy_Communities/src/main/java/Energy_Communities_Group/A/Energy_Communities/EnergyCommunitiesApplication.java
@@ -0,0 +1,13 @@
+package Energy_Communities_Group.A.Energy_Communities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyCommunitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyCommunitiesApplication.class, args);
+	}
+
+}
---
+++ Energy_Communities/src/test/java/Energy_Communities_Group/A/Energy_Communities/EnergyCommunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package Energy_Communities_Group.A.Energy_Communities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyCommunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
