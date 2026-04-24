9f139c83df17cdc7c5675def91a478d3a3c20867
Thomas-xyz <thomas.steiner@inki.at>
2026-04-24 16:18:33 +0200

    added Current Percentage and the Repository

--- energy-javafx-gui/src/main/java/module-info.java
+++ energy-javafx-gui/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.energyjavafxgui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires spring.boot;
 
 
     opens org.example.energyjavafxgui to javafx.fxml;
--- energy-rest-api/src/main/java/Energy_Communities_Group/A/Energy_Communities/CurrentPercentage.java
+++ energy-rest-api/src/main/java/Energy_Communities_Group/A/Energy_Communities/CurrentPercentage.java
@@ -2,12 +2,20 @@ package Energy_Communities_Group.A.Energy_Communities;
 
 public class CurrentPercentage {
     private String hour;
-    private double community_depleted;
-    private double grid_portion;
+    private double communityDepleted;
+    private double gridPortion;
 
-    public CurrentPercentage(double grid_portion, double community_depleted, String hour) {
-        this.grid_portion = grid_portion;
-        this.community_depleted = community_depleted;
+    public CurrentPercentage(String hour, double communityDepleted, double gridPortion) {
         this.hour = hour;
+        this.communityDepleted = communityDepleted;
+        this.gridPortion = gridPortion;
     }
+
+    public String getHour() {return hour;}
+    public double getCommunityDepleted(){return communityDepleted;}
+    public double getGridPortion(){return gridPortion;}
+
+    public void setHour(String hour){this.hour = hour;}
+    public void setCommunityDepleted(double communityDepleted){this.communityDepleted = communityDepleted;}
+    public void setGridPortion(double gridPortion){this.gridPortion = gridPortion;}
 }
