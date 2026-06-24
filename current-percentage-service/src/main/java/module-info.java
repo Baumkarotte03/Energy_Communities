97777a0139df0f3b60a0b07fa571c1d28a5393ae
Thomas-xyz <thomas.steiner@inki.at>
2026-06-24 18:27:36 +0200

    copied the program from current-percentage-service because it doesnt work there. lukas had some problems. here it runs normally

--- current-percentage-service/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.currentpercentageservice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.currentpercentageservice to javafx.fxml;
-    exports org.example.currentpercentageservice;
-}
---
+++ energy-user/src/main/java/org/example/energyuser/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package org.example.energyuser;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String ENERGY_QUEUE = "energy.queue";
+
+    @Bean
+    public Queue energyQueue() {
+        return new Queue(ENERGY_QUEUE, true);
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+}
